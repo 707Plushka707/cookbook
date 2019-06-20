55c623f56eb322c11e14cf21cde2b55315dfc9d3
elbashery <ahmedalbachiry@gmail.com>
2019-06-20 22:08:17 +0300

    adding how to save data on mongo from form client

---
+++ form/form.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+    "fmt"
+    "os"
+
+    "gopkg.in/mgo.v2"
+)
+
+type Book struct {
+	Name   string
+	Subject string
+	Author string
+}
+
+func main() {
+    session, err := mgo.Dial("localhost:27017")
+    CheckeErr("err in create session: ", err)
+
+    defer session.Close()
+
+    tmpl := template.Must(template.ParseFiles("asset/form.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+        c := session.DB("test").C("Book")
+
+		book := Book{
+			Name:   r.FormValue("name"),
+			Subject: r.FormValue("subject"),
+            Author: r.FormValue("author"),
+		}
+        err = c.Insert(&Book{book.Name, book.Subject, book.Author})
+
+		// do something with details
+        fmt.Println(book.Name)
+        fmt.Println(book.Subject)
+
+		tmpl.Execute(w, struct{ Success bool }{true})
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func CheckeErr(str string, err error) {
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(0)
+    }
+}
