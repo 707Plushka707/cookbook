bceb0575c56f43b714e26a20d27637b6bccde62b
elbashery <ahmedalbachiry@gmail.com>
2019-06-25 17:21:37 +0300

    access to variable

--- template.go
+++ template.go
@@ -12,9 +12,22 @@ type Person struct {
 
 func main() {
     p := Person{Name: "admin", Age:"34"}
-    t := template.New("nonexported template demo")
-    t, _ = t.Parse("hello {{.Name}}! Age is {{.Age}}.")
+
+    // access to field in struct by point like this : {{.Feild}} 
+    t := template.New("text 0")
+    t, _ = t.Parse("hello {{.Name}}! Age is {{.Age}}.\n")
     err := t.Execute(os.Stdout, p)
+
+    if err != nil {
+        fmt.Println("ther was an error:", err)
+    }
+
+    name := "adam"
+    // access to value of varialbl just by point like this : {{.}} 
+    t1 := template.New("example 1")
+    t1, _ = t1.Parse("Hi! {{if `true`}} {{.}} {{else}} Evryone {{end}}")
+    err = t1.Execute(os.Stdout, name)
+
     if err != nil {
         fmt.Println("ther was an error:", err)
     }
