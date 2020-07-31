80e731f65e42b8f6ccafbc88d40fd8483e4901fe
elbashery <ahmedalbachiry@gmail.com>
2020-07-31 23:58:08 +0300

    new commit

--- template/template.go
+++ template/template.go
@@ -1,45 +1,43 @@
 package main
+
 import (
-    "fmt"
-    "text/template"
-    "os"
+	"fmt"
+	"os"
+	"text/template"
 )
 
 type Person struct {
-    Name string
-    Age string
+	Name string
+	Age  string
 }
 
 func main() {
-    fmt.Printf("%%")
-    p := Person{Name: "admin", Age:"34"}
+	fmt.Printf("%%\n")
+	p := Person{Name: "admin", Age: "34"}
 
-    // access to field in struct by point like this : {{.Feild}} 
-    t := template.New("text 0")
-    t, _ = t.Parse("hello {{.Name}}! Age is {{.Age}}.\n")
-    err := t.Execute(os.Stdout, p)
-    checkeErr("err when parsing struct", err)
+	// access to field in struct by point like this : {{.Feild}}
+	t := template.New("text 0")
+	t, _ = t.Parse("hello {{.Name}}! Age is {{.Age}}.\n")
+	err := t.Execute(os.Stdout, p)
+	checkeErr("err when parsing struct", err)
 
+	name := "adam"
+	// access to value of varialbl just by point like this : {{.}}
+	t1 := template.New("example 1")
+	t1, _ = t1.Parse("Hi! {{if `true`}} {{.}} {{else}} Evryone {{end}}")
+	err = t1.Execute(os.Stdout, name)
+	checkeErr("err when parsing variable", err)
 
-    name := "adam"
-    // access to value of varialbl just by point like this : {{.}} 
-    t1 := template.New("example 1")
-    t1, _ = t1.Parse("Hi! {{if `true`}} {{.}} {{else}} Evryone {{end}}")
-    err = t1.Execute(os.Stdout, name)
-    checkeErr("err when parsing variable", err)
-
-
-    slc := []int{2, 3, 4, 5, 6}
-    t2 := template.New("example 1")
-    t2, _ = t1.Parse("range  {{range .}} {{.}} {{end}}")
-    err = t2.Execute(os.Stdout, slc)
-    checkeErr("err when parsing slice", err)
+	slc := []int{2, 3, 4, 5, 6}
+	t2 := template.New("example 1")
+	t2, _ = t1.Parse("range  {{range .}} {{.}} {{end}}")
+	err = t2.Execute(os.Stdout, slc)
+	checkeErr("err when parsing slice", err)
 }
 
 func checkeErr(str string, err error) {
-    if err != nil {
-        fmt.Println(str, err)
-        os.Exit(0)
-    }
+	if err != nil {
+		fmt.Println(str, err)
+		os.Exit(0)
+	}
 }
-
