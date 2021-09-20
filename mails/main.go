8a36f16d22073a06acc0f065a53463ae0f88860d
bashery <ahmedalbachiry@gmail.com>
2021-09-20 14:35:17 +0300

    new examples for flutter

--- mails/main.go
+++ mails/main.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	msg := mail.NewMessage()
-	msg.SetHeader("From", "Ahmed@gmail.com")
-	msg.SetHeader("To", "Ahmedin@gmail.com")
+	msg.SetHeader("From", "lakadkldlk@gmail.com")
+	msg.SetHeader("To", "hwaddy@protonmail.com")
 	msg.SetHeader("Subject", "test golang email")
 	msg.SetBody("text/html", "<h2>this is an fucking spam email</h2>")
 
-	d := mail.NewDialer("smtp.gmail.com", 587, "ahmed@email.com", "mySerperSecretPass") // .env
-	d.StartTLSPolicy = mail.MandatoryStartTLS
+	d := mail.NewDialer("smtp.gmail.com", 587, "ahmedalbachiry@gmail.com", "myMoreSecurePass") // .env
+	//d.StartTLSPolicy = mail.MandatoryStartTLS
 
 	err := d.DialAndSend(msg)
 	if err != nil {
