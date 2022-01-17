86cd4718f58bed6fc830c233f815a3cac75feb0b
bashery <ahmedalbachiry@gmail.com>
2022-01-17 09:44:01 +0300

    new file:   fyneapp/app.go 	new file:   webview/go.mod 	new file:   webview/go.sum 	new file:   webview/simple.html 	new file:   webview/view.go

---
+++ webview/go.mod
@@ -0,0 +1,5 @@
+module webvew
+
+go 1.17
+
+require github.com/webview/webview v0.0.0-20210330151455-f540d88dde4e
---
+++ webview/view.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/webview/webview"
+
+func main() {
+	debug := false
+	w := webview.New(debug)
+	defer w.Destroy()
+	w.SetTitle("Minimal webview example")
+	w.SetSize(800, 600, webview.HintNone)
+    w.Navigate("file:///Users/fedora/repo/cookbook/webview/simple.html")
+	w.Run()
+
+}
